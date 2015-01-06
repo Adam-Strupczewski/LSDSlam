1a30cf162f2cc2e69392f234a832474aefa0d9c9
Adam Strupczewski <adam.strupczewski@gmail.com>
2015-01-06 14:21:30 +0100

    Improved display

--- src/main_on_images.cpp
+++ src/main_on_images.cpp
@@ -46,6 +46,8 @@
 #include <QFuture>
 #include <QtConcurrent/QtConcurrent>
 
+#define USE_CAMERA false
+
 // Viewer for 3D reconstructed scene, uses qglviewer
 PointCloudViewer *viewer;
 
@@ -154,9 +156,6 @@ int main( int argc, char** argv )
     viewer = new PointCloudViewer();
     viewer->setWindowTitle("PointCloud Viewer");
 
-    // Make the viewer window visible on screen.
-    viewer->show();
-
     display1 = new QGLDisplay();
     display2 = new QGLDisplay();
     display3 = new QGLDisplay();
@@ -166,6 +165,24 @@ int main( int argc, char** argv )
     display2->setWindowTitle("Tracking Residual");
     display3->setWindowTitle("Stereo Keyframe");
     display4->setWindowTitle("Stereo Reference Frame");
+
+    int sizew = 500;
+    int sizeh = 360;
+
+    display1->resize(sizew, sizeh);
+    display1->move(0,0);
+    display2->resize(sizew, sizeh);
+    display2->move(0,1.2*sizeh);
+    display3->resize(sizew, sizeh);
+    display3->move(1.2*sizew,0);
+    display4->resize(sizew, sizeh);
+    display4->move(1.2*sizew,1.2*sizeh);
+
+    viewer->resize(sizew, sizeh);
+    viewer->move(2.4*sizew, 2.4*sizeh);
+
+    // Make the viewer window visible on screen.
+    viewer->show();
 
     display1->show();
     display2->show();
@@ -177,186 +194,6 @@ int main( int argc, char** argv )
     //future.waitForFinished();
     //return 0;
     return application.exec();
-/*
-//	ros::init(argc, argv, "LSD_SLAM");
-
-//	dynamic_reconfigure::Server<lsd_slam_core::LSDParamsConfig> srv(ros::NodeHandle("~"));
-//	srv.setCallback(dynConfCb);
-
-//	dynamic_reconfigure::Server<lsd_slam_core::LSDDebugParamsConfig> srvDebug(ros::NodeHandle("~Debug"));
-//	srvDebug.setCallback(dynConfCbDebug);
-
-//	packagePath = ros::package::getPath("lsd_slam_core")+"/";
-
-    // get camera calibration in form of an undistorter object.
-    // if no undistortion is required, the undistorter will just pass images through.
-    std::string calibFile;
-    Undistorter* undistorter = 0;
-
-//	if(ros::param::get("~calib", calibFile))
-//	{
-//		 undistorter = Undistorter::getUndistorterForFile(calibFile.c_str());
-//		 ros::param::del("~calib");
-//	}
-    undistorter = Undistorter::getUndistorterForFile("/home/adam/dokt_ws/LSD_machine_small/cameraCalibration.cfg");
-
-	if(undistorter == 0)
-	{
-		printf("need camera calibration file! (set using _calib:=FILE)\n");
-		exit(0);
-	}
-
-	int w = undistorter->getOutputWidth();
-	int h = undistorter->getOutputHeight();
-
-	int w_inp = undistorter->getInputWidth();
-	int h_inp = undistorter->getInputHeight();
-
-	float fx = undistorter->getK().at<double>(0, 0);
-	float fy = undistorter->getK().at<double>(1, 1);
-	float cx = undistorter->getK().at<double>(2, 0);
-	float cy = undistorter->getK().at<double>(2, 1);
-	Sophus::Matrix3f K;
-	K << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0;
-
-    // Create QGLViewer in a separate thread
-    //std::thread t1(initQGLViewer);
-    //initQGLViewer();
-    //t1.join();
-
-    //system->initVisualization();
-
-    // Wait until PointcloudViewer is created - TODO
-    // ..............................................................
-    usleep(1000000);
-
-    // make output wrapper. just set to zero if no output is required.
-    //Output3DWrapper* outputWrapper = NULL;// = new ROSOutput3DWrapper(w,h);
-    Output3DWrapper* outputWrapper = new ROSOutput3DWrapper(w,h);
-
-    // Set pointcloudviewer pointer in OutputWrapper
-    outputWrapper->setViewer(viewer);
-
-    // make slam system
-    SlamSystem* system = new SlamSystem(w, h, K, doSlam);
-    system->setVisualization(outputWrapper);
-
-	// open image files: first try to open as file.
-	std::string source;
-	std::vector<std::string> files;
-
-//	if(!ros::param::get("~files", source))
-//	{
-//		printf("need source files! (set using _files:=FOLDER)\n");
-//		exit(0);
-//	}
-//	ros::param::del("~files");
-    source = "/home/adam/dokt_ws/LSD_machine_small/images";
-
-	if(getdir(source, files) >= 0)
-	{
-		printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
-	}
-	else if(getFile(source, files) >= 0)
-	{
-		printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
-	}
-	else
-	{
-		printf("could not load file list! wrong path / file?\n");
-	}
-
-
-
-    // get HZ
-    double hz = 0;
-//	if(!ros::param::get("~hz", hz))
-//		hz = 0;
-//	ros::param::del("~hz");
-
-
-
-    cv::Mat image = cv::Mat(h,w,CV_8U);
-    int runningIDX=0;
-    float fakeTimeStamp = 0;
-
-//	ros::Rate r(hz);
-
-//    QApplication application(argc, argv);
-//    PointCloudViewer* viewer = 0;
-
-//    // Instantiate the viewer of the scene reconstruction.
-//    viewer = new PointCloudViewer();
-//    viewer->setWindowTitle("PointCloud Viewer");
-
-//    // Make the viewer window visible on screen.
-//    viewer->show();
-
-	for(unsigned int i=0;i<files.size();i++)
-	{
-        //application.processEvents();
-
-        printf("Processing image %s!\n", files[i].c_str());
-
-		cv::Mat imageDist = cv::imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
-
-		if(imageDist.rows != h_inp || imageDist.cols != w_inp)
-		{
-			if(imageDist.rows * imageDist.cols == 0)
-				printf("failed to load image %s! skipping.\n", files[i].c_str());
-			else
-				printf("image %s has wrong dimensions - expecting %d x %d, found %d x %d. Skipping.\n",
-						files[i].c_str(),
-						w,h,imageDist.cols, imageDist.rows);
-			continue;
-		}
-		assert(imageDist.type() == CV_8U);
-
-		undistorter->undistort(imageDist, image);
-		assert(image.type() == CV_8U);
-
-		if(runningIDX == 0)
-			system->randomInit(image.data, fakeTimeStamp, runningIDX);
-		else
-			system->trackFrame(image.data, runningIDX ,hz == 0,fakeTimeStamp);
-		runningIDX++;
-		fakeTimeStamp+=0.03;
-
-        // Is supposed to make the loop run at the desired frequency, sleep occurs for leftover time from cycle
-//		if(hz != 0)
-//			r.sleep();
-
-		if(fullResetRequested)
-		{
-
-			printf("FULL RESET!\n");
-			delete system;
-
-			system = new SlamSystem(w, h, K, doSlam);
-			system->setVisualization(outputWrapper);
-
-			fullResetRequested = false;
-			runningIDX = 0;
-		}
-
-        // Added by AS
-//      boost::this_thread::sleep( boost::posix_time::milliseconds(50) );
-
-        // Process callbacks here
-//		ros::spinOnce();
-
-//		if(!ros::ok())
-//			break;
-
-	}
-
-	system->finalize();
-
-	delete system;
-	delete undistorter;
-	delete outputWrapper;
-
-    return 0;*/
 }
 
 int mainLoopCodeForQtThread()
@@ -403,23 +240,39 @@ int mainLoopCodeForQtThread()
     std::string source;
     std::vector<std::string> files;
 
-    source = "/home/adam/dokt_ws/LSD_machine/images";
+    // Use camera
+    if (USE_CAMERA){
+        cv::VideoCapture webcam(0);
+        if(!webcam.isOpened())
+        {
+            printf("Error: cannot open stream from webcam\n");
+            cv::waitKey(0);
+            return -1;
+        }
+        webcam.set(CV_CAP_PROP_FRAME_WIDTH,w_inp);
+        webcam.set(CV_CAP_PROP_FRAME_HEIGHT,h_inp);
 
-    if(getdir(source, files) >= 0)
-    {
-        printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
-    }
-    else if(getFile(source, files) >= 0)
-    {
-        printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
-    }
-    else
-    {
-        printf("could not load file list! wrong path / file?\n");
-    }
+        cv::Mat frame;
+        while (webcam.read(frame))
+        {
+            outputWrapper->showKeyframeDepth(frame);
+        }
+    }else{
+        source = "/home/adam/dokt_ws/LSD_machine/images";
 
-    // get HZ
-    double hz = 0;
+        if(getdir(source, files) >= 0)
+        {
+            printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
+        }
+        else if(getFile(source, files) >= 0)
+        {
+            printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
+        }
+        else
+        {
+            printf("could not load file list! wrong path / file?\n");
+        }
+    }
 
     cv::Mat image = cv::Mat(h,w,CV_8U);
     int runningIDX=0;
@@ -449,7 +302,7 @@ int mainLoopCodeForQtThread()
         if(runningIDX == 0)
             system->randomInit(image.data, fakeTimeStamp, runningIDX);
         else
-            system->trackFrame(image.data, runningIDX ,hz == 0,fakeTimeStamp);
+            system->trackFrame(image.data, runningIDX ,true,fakeTimeStamp);
         runningIDX++;
         fakeTimeStamp+=0.03;
 
